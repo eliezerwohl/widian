439e78528c3378883b755318006557616c42c50b
Eliezer Wohl <eliezerwohl@gmail.com>
2018-04-04 18:18:35 -0400

    animated scrolling on page

--- footer.php
+++ footer.php
@@ -22,5 +22,6 @@
 	<!-- <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script> -->
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/faq.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/making.js"></script>
 </body>
 </html>
---
+++ js/making.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+  $(".fa-angle-up").on("click", function(e){
+    e.preventDefault();
+    $('html, body').animate({
+		    scrollTop: 0
+		}, 1000);
+  })
+  $(".service-section a").on("click", function(e){
+    e.preventDefault();
+    var top = $(this.hash).offset().top
+    $('html, body').animate({
+        scrollTop: top
+    }, 1000);
+  })
+});
